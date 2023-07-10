6f369fdc1f3fd6c9e2c2a5fac595279992e35435
ANTHONY WOO <anthony.woo14@gmail.com>
2023-07-10 17:08:58 -0600

    api endpoint to get event by id

--- api/queries/events.py
+++ api/queries/events.py
@@ -1,12 +1,20 @@
 from .client import Queries
 from models import EventIn, EventOut
 
-class EventQueries(Queries):
-  DB_NAME = "animos-db"
-  COLLECTION = "accounts"
 
-  def create(self, event: EventIn) -> EventOut:
-    props = event.dict()
-    self.collection.insert_one(props)
-    props["id"] = str(props["_id"])
-    return EventOut(**props)
+class EventQueries(Queries):
+    DB_NAME = "animos-db"
+    COLLECTION = "accounts"
+
+    def create(self, event: EventIn) -> EventOut:
+        props = event.dict()
+        self.collection.insert_one(props)
+        props["id"] = str(props["_id"])
+        return EventOut(**props)
+
+    def get(self, id) -> EventOut:
+        result = self.collection.find_one(id)
+        if result is None:
+            raise Exception("Event Not Found")
+        result["id"] = str(result["_id"])
+        return EventOut(**result)
--- api/routers/events.py
+++ api/routers/events.py
@@ -5,10 +5,20 @@ from bson import ObjectId
 
 router = APIRouter()
 
+
 @router.post("/api/events", response_model=EventOut)
 def create_event(
-  event: EventIn,
-  repo: EventQueries = Depends()
+    event: EventIn,
+    repo: EventQueries = Depends()
 ):
-  event = repo.create(event)
-  return event
+    event = repo.create(event)
+    return event
+
+
+@router.get("/api/events/{id}", response_model=EventOut)
+def get_event(
+    id: str,
+    repo: EventQueries = Depends()
+):
+    event = repo.get({"_id": ObjectId(id)})
+    return event
