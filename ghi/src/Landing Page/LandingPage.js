a7cc037a924bba1445bf2274f316c07a41518019
Samantha Chin <samanthachin.dsp@gmail.com>
2023-07-12 10:06:52 -0400

    worked on landing page navbar

---
+++ ghi/src/Landing Page/LandingPage.js
@@ -0,0 +1,17 @@
+import './LandingPage.css';
+import LandingPageNav from './LandingPageNav.js';
+
+
+function LandingPage(){
+    return (
+      <>
+        <LandingPageNav />
+        <div class="container">
+          <h1>Test H1</h1>
+          <h2>Test H2</h2>
+          <h3>Test H3</h3>
+        </div>
+      </>
+    );
+}
+export default LandingPage;
