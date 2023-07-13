818820dc1adfcea92a2ee76918dcb7661e0419e8
Samantha Chin <samanthachin.dsp@gmail.com>
2023-07-13 19:34:10 +0000

    Frontend - Landing Page with Nav & Styling Buttons (v2)

--- ghi/src/Landing Page/LandingPage.js
+++ ghi/src/Landing Page/LandingPage.js
@@ -1,16 +1,39 @@
 import './LandingPage.css';
 import LandingPageNav from './LandingPageNav.js';
-import Logo from '../assets/images/animoslogo.png'
+import Logo from '../assets/images/animoslogo.png';
+import Photo from '../assets/images/landingpage_photo.png'
 
 function LandingPage(){
     return (
       <>
         <LandingPageNav />
-        <div class="container">
-          <h1>Test H1</h1>
-          <h2>Test H2</h2>
-          <img class="logo" src={Logo}></img>
-          <h3>Test H3</h3>
+        <div className="landingpage__container">
+          <div className="grid">
+            <div>
+              <div className="logo_subtitle">
+                <img className="logo" src={Logo}></img>
+                <h4>
+                  Expand your furry friend’s social circle
+                  <br />
+                  and gather at your next pet-outing.
+                </h4>
+              </div>
+              <div className="buttons">
+              <button className="get_started_button button">
+                <a className="nav-link nav-link-signup" aria-current="page" href="/signup">
+                Get Started
+                </a></button>
+              <button className="login_button button">
+                <a className="nav-link nav-link-signup" aria-current="page" href="/login">
+                Login
+                </a>
+                </button>
+              </div>
+            </div>
+            <img className="photo" src={Photo}></img>
+            <div className="content">
+            </div>
+          </div>
         </div>
       </>
     );
