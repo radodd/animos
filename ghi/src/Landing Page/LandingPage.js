43ac16ba1af56bdf9a19bc7184b25ca2d30fbdb7
PA <epinette.pa@gmail.com>
2023-07-13 18:39:59 -0700

    Revert "frontend signup and login"
    This reverts commit 2868826ecf66e7b56b2f6f86f088ae8cc2b3f5a4.

--- ghi/src/Landing Page/LandingPage.js
+++ ghi/src/Landing Page/LandingPage.js
@@ -1,18 +1,18 @@
-import "./LandingPage.css";
-import LandingPageNav from "./LandingPageNav.js";
-import Logo from "../assets/images/animoslogo.png";
+import './LandingPage.css';
+import LandingPageNav from './LandingPageNav.js';
+import Logo from '../assets/images/animoslogo.png'
 
-function LandingPage() {
-  return (
-    <>
-      <LandingPageNav />
-      <div className="container">
-        <h1>Test H1</h1>
-        <h2>Test H2</h2>
-        <img className="logo" src={Logo}></img>
-        <h3>Test H3</h3>
-      </div>
-    </>
-  );
+function LandingPage(){
+    return (
+      <>
+        <LandingPageNav />
+        <div class="container">
+          <h1>Test H1</h1>
+          <h2>Test H2</h2>
+          <img class="logo" src={Logo}></img>
+          <h3>Test H3</h3>
+        </div>
+      </>
+    );
 }
 export default LandingPage;
