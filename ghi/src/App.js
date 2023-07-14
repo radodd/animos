ae90f65912df36f443b77d98edad793a4e52022d
anthony-t-woo <anthony.woo14@gmail.com>
2023-07-14 21:03:07 +0000

    Event detail

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,66 +1,112 @@
-import { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
-import EventsList from "./EventsList/EventsList.js";
+import { useEffect, useState } from 'react';
+import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
 import LandingPage from './LandingPage/LandingPage.js';
+import EventsList from './EventsList/EventsList.js';
+import EventDetail from './Event Detail/eventDetail.js';
 import CreateEvent from './Create Event/createEvent.js';
 import LocationsListDetail from './LocationsListDetail/LocationsListDetail.js';
-// import Construct from "./Construct.js";
-// import ErrorNotification from "./ErrorNotification";
 import './App.css';
 
 function App() {
-  const [events, setEvents] = useState([]);
-  const [locations, setLocations] = useState([]);
-  const { id } = useParams();
-  // const [launchInfo, setLaunchInfo] = useState([]);
-  // const [error, setError] = useState(null);
+    const [locations, setLocations] = useState([]);
+    const [events, setEvents] = useState([]);
+    const [event, setEvent] = useState({});
+    const [location, setLocation] = useState({});
 
-  async function loadLocations() {
-    const response = await fetch("http://localhost:8000/api/locations/");
-    if (response.ok) {
-      const data = await response.json();
-      setLocations(data.locations);
-    } else {
-      console.error(response);
+    async function loadEvent(id) {
+        const response = await fetch(`http://localhost:8000/api/events/${id}`);
+        if (response.ok) {
+            const data = await response.json();
+            const location_data = await loadLocation(data.location_id);
+            data.location = location_data;
+            const user_id = 'test account';
+            if (data.account_id === user_id) {
+                data.is_owner = true;
+            } else {
+                data.is_owner = false;
+            }
+            setEvent(data);
+            setLocation(data.location);
+        }
     }
-  }
 
-  async function getEvents() {
-    const response = await fetch("http://localhost:8000/api/events");
-    if (response.ok) {
-      const data = await response.json();
-      setEvents(data.events);
+    async function loadLocation(id) {
+        const response = await fetch(
+            `http://localhost:8000/api/locations/${id}`
+        );
+        if (response.ok) {
+            const data = await response.json();
+            return data;
+        }
     }
-  }
 
-  useEffect(() => {
-    loadLocations();
-    getEvents();
-  }, []);
+    async function loadLocations() {
+        const response = await fetch('http://localhost:8000/api/locations/');
+        if (response.ok) {
+            const data = await response.json();
+            setLocations(data.locations);
+        } else {
+            console.error(response);
+        }
+    }
 
-  return (
-    <div>
-      <BrowserRouter>
-        {/* <Nav /> */}
-        <div className="container">
-          <Routes>
-            <Route path="/" element={<LandingPage />} />
-            <Route path="locations">
-              <Route index element={<LocationsListDetail locations={locations} />}/>
-            </Route>
-            <Route path="events">
-              <Route index element={<EventsList events={events} />} />
-              <Route path="create" element={<CreateEvent locations={locations} />}
-              />
-              {/* <Route path=":id" element={<EventDetail id={id} />} /> */}
-            </Route>
-          </Routes>
-        </div>
-        {/* <ErrorNotification error={error} />
+    async function getEvents() {
+        const response = await fetch('http://localhost:8000/api/events');
+        if (response.ok) {
+            const data = await response.json();
+            setEvents(data.events);
+        }
+    }
+
+    useEffect(() => {
+        loadLocations();
+        getEvents();
+    }, []);
+
+    return (
+        <div>
+            <BrowserRouter>
+                {/* <Nav /> */}
+                <div className="container">
+                    <Routes>
+                        <Route path="/" element={<LandingPage />} />
+                        <Route path="locations">
+                            <Route
+                                index
+                                element={
+                                    <LocationsListDetail
+                                        locations={locations}
+                                    />
+                                }
+                            />
+                        </Route>
+                        <Route path="events">
+                            <Route
+                                index
+                                element={<EventsList events={events} />}
+                            />
+                            <Route
+                                path="create"
+                                element={<CreateEvent locations={locations} />}
+                            />
+                            <Route
+                                path=":id"
+                                element={
+                                    <EventDetail
+                                        loadEvent={loadEvent}
+                                        event={event}
+                                        location={location}
+                                    />
+                                }
+                            />
+                        </Route>
+                    </Routes>
+                </div>
+                {/* <ErrorNotification error={error} />
         <Construct info={launchInfo} /> */}
-      </BrowserRouter>
-    </div>
-  );
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
--- ghi/src/LocationsListDetail/LocationsListDetail.js
+++ ghi/src/LocationsListDetail/LocationsListDetail.js
@@ -2,67 +2,77 @@ import { useState } from 'react';
 import './LocationListDetail.css';
 
 function LocationsList({ locations }) {
-  const [activeModal, setActiveModal] = useState(null);
-  const toggleModal = (index) => {
-    setActiveModal(index === activeModal ? null: index);
-  };
+    const [activeModal, setActiveModal] = useState(null);
+    const toggleModal = (index) => {
+        setActiveModal(index === activeModal ? null : index);
+    };
 
-  function Card() {
+    function Card() {
+        return (
+            <>
+                {locations.map((location, index) => (
+                    <div className="location-card" key={location.id}>
+                        <div className="card-body">
+                            <img
+                                className="card-image"
+                                width="100%"
+                                src={location.picture_url}
+                            />
+                            <h5 className="card-title">{location.name}</h5>
+                            <button
+                                className="card-button"
+                                onClick={() => toggleModal(index)}
+                            >
+                                View details
+                            </button>
+                        </div>
+                    </div>
+                ))}
+                {locations.map((location, index) => (
+                    <div
+                        className={` ${
+                            activeModal === index
+                                ? 'active-modal location_modal'
+                                : 'location_modal'
+                        }`}
+                        key={location.id}
+                    >
+                        <div className="location_modal-content">
+                            <button
+                                className="location_modal-close-button"
+                                onClick={() => toggleModal(index)}
+                            >
+                                X
+                            </button>
+                            <img
+                                className="modal-image"
+                                width="100%"
+                                src={location.picture_url}
+                            />
+                            <h5 className="modal-title">{location.name}</h5>
+                            <p className="modal-zipcode">{location.zipcode}</p>
+                            <p className="modal-capacity">
+                                Capacity: {location.capacity}
+                            </p>
+                            <p className="modal-description">
+                                {location.description}
+                            </p>
+                        </div>
+                    </div>
+                ))}
+            </>
+        );
+    }
+
     return (
-      <>
-        {locations.map((location, index) => (
-          <div className="location-card">
-            <div className="card-body">
-              <img
-                className="card-image"
-                width="100%"
-                src={location.picture_url}
-              />
-              <h5 className="card-title">{location.name}</h5>
-              <button
-                className="card-button"
-                onClick={() => toggleModal(index)}
-              >
-                View details
-              </button>
+        <>
+            <h1>Placeholder for NavBar</h1>
+            <h1> Pet Furiendly Locations 🐾</h1>
+            <div className="wrapper">
+                <Card />
             </div>
-          </div>
-        ))}
-        {locations.map((location, index) => (
-          <div className={` ${activeModal === index ?
-              'active-modal location_modal' : 'location_modal'}`}
-            key={location.id}>
-            <div className="location_modal-content">
-              <button
-                className="location_modal-close-button"
-                onClick={() => toggleModal(index)}
-              >X</button>
-              <img
-                className="modal-image"
-                width="100%"
-                src={location.picture_url}
-              />
-              <h5 className="modal-title">{location.name}</h5>
-              <p className="modal-zipcode">{location.zipcode}</p>
-              <p className="modal-capacity">Capacity: {location.capacity}</p>
-              <p className="modal-description">{location.description}</p>
-            </div>
-          </div>
-        ))}
-      </>
+        </>
     );
-  }
-
-  return (
-    <>
-
-      <h1>Placeholder for NavBar</h1>
-      <h1> Pet Furiendly Locations 🐾</h1>
-      <div className="wrapper">
-        <Card />
-      </div>
-    </>
-  );
 }
 
 export default LocationsList;
