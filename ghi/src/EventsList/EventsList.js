ae857ff16f8141e30e34a027e58e2cd31b5e5487
Erick Watanabe <erickjwatanabe@gmail.com>
2023-07-17 12:40:56 -0700

    Modals displaying when event details button is clicked. cards displaying css

--- ghi/src/EventsList/EventsList.js
+++ ghi/src/EventsList/EventsList.js
@@ -1,39 +1,92 @@
 import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import "./EventsList.css";
 
 function EventsList(props) {
+  const [activeModal, setActiveModal] = useState(null);
+  const toggleModal = (index) => {
+    setActiveModal(index === activeModal ? null : index);
+  };
+  function Card() {
+    return (
+      <>
+        <div className="events-list">
+          {props.events.map((event, index) => {
+            const date = new Date(event.date_start).toLocaleDateString();
+            const time = new Date(event.date_start).toLocaleTimeString();
+            let locationName;
+            props.locations.map((location) => {
+              if (event.location_id === location.id) {
+                locationName = location.name;
+              }
+            });
+
+            return (
+              <div className="event-card" key={event.id}>
+                <div className="card-title">{event.name}</div>
+                <img className="card-image" src={event.picture_url}></img>
+                <div className="card-date-start">Date: {date}</div>
+                <div className="card-time-start">Start time: {time}</div>
+                <div className="card-location">Where: {locationName}</div>
+                <div className="card-description">{event.description}</div>
+                <button
+                  className="card-button"
+                  onClick={() => toggleModal(index)}
+                >
+                  Event Details
+                </button>
+              </div>
+            );
+          })}
+          {props.events.map((event, index) => {
+            const date = new Date(event.date_start).toLocaleDateString();
+            const time = new Date(event.date_start).toLocaleTimeString();
+            let locationName;
+            props.locations.map((location) => {
+              if (event.location_id === location.id) {
+                locationName = location.name;
+              }
+            });
+
+            return (
+              <div
+                key={event.id}
+                className={`${
+                  activeModal === index
+                    ? "active-modal location_modal"
+                    : "location_modal"
+                }`}
+              >
+                <div className="modal-title">{event.name}</div>
+                <img className="modal-image" src={event.picture_url}></img>
+                <div className="modal-date-start">date: {date}</div>
+                <div className="modal-time-start">start time: {time}</div>
+                <div className="modal-location">
+                  This event is happening at {locationName}
+                </div>
+                <div className="card-description">{event.description}</div>
+                <button
+                  className="modal-button"
+                  onClick={() => toggleModal(index)}
+                >
+                  Close
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      </>
+    );
+  }
+
   return (
     <>
       <h1>Events Happening</h1>
       <NavLink type="button" to="/events/create">
         Create Event
       </NavLink>
-      <div className="events-List">
-        {props.events.map((event) => {
-          const date = new Date(event.date_start).toLocaleDateString();
-          const time = new Date(event.date_start).toLocaleTimeString();
-          let locationName;
-          props.locations.map((location) => {
-            if (event.location_id === location.id) {
-              locationName = location.name;
-            }
-          });
-
-          return (
-            <div className="event" key={event.id}>
-              <div className="event-name">{event.name}</div>
-              <img className="picture-url" src={event.picture_url}></img>
-              <div className="event-date-start">date: {date}</div>
-              <div className="event-date-start">start time: {time}</div>
-              <div className="event-location">
-                This event is happening at {locationName}
-              </div>
-              <div className="event-description">{event.description}</div>
-              <NavLink type="button" to={`/events/${event.id}`}>
-                Event Details
-              </NavLink>
-            </div>
-          );
-        })}
+      <div className="wrapper">
+        <Card />
       </div>
     </>
   );
