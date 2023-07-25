89055746fa9f7232f2ebd233c817775c6d96d1b4
Samantha Chin <samanthachin.dsp@gmail.com>
2023-07-25 11:23:26 -0700

    Remove console log

--- ghi/src/NavBar.js
+++ ghi/src/NavBar.js
@@ -2,7 +2,6 @@ import { useSelector } from 'react-redux';
 
 export default function NavBar() {
     const user = useSelector((state) => state.user);
-    console.log(user)
         return (
           <>
             <div className="container-nav">
