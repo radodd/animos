a32e6b9fb9106649baa18ec2d25bae249887f0ad
Samantha Chin <samanthachin.dsp@gmail.com>
2023-07-25 07:27:24 -0700

    revisions to navbar redirects

--- ghi/src/NavBar.js
+++ ghi/src/NavBar.js
@@ -4,79 +4,75 @@ export default function NavBar() {
     const user = useSelector((state) => state.user);
     if (user != null) {
         return (
-            <>
-                <div className="container-nav">
-                    <header className="header">
-                        <nav className="navbar navbar-expand navbar-light">
-                            <h1> Welcome, {user.first_name}</h1>
-                            <button
-                                className="navbar-toggler ms-auto"
-                                type="button"
-                                data-bs-toggle="collapse"
-                                data-bs-target="#navbarSupportedContent"
-                                aria-controls="navbarSupportedContent"
-                                aria-expanded="false"
-                                aria-label="Toggle navigation"
-                            >
-                                <span className="navbar-toggler-icon"></span>
-                            </button>
+          <>
+            <div className="container-nav">
+              <header className="header">
+                <nav className="navbar navbar-expand navbar-light">
+                  <h1> Welcome, {user.first_name}</h1>
+                  <button
+                    className="navbar-toggler ms-auto"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                  >
+                    <span className="navbar-toggler-icon"></span>
+                  </button>
 
-                            <div
-                                className="collapse navbar-collapse"
-                                id="navbarSupportedContent"
-                            >
-                                <ul className="navbar-nav ms-auto">
-                                    <li className="nav-item">
-                                        <a
-                                            className="nav-link"
-                                            aria-current="page"
-                                            href="/events"
-                                        >
-                                            Events
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a
-                                            className="nav-link"
-                                            aria-current="page"
-                                            href="/locations"
-                                        >
-                                            Locations
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a
-                                            className="nav-link"
-                                            aria-current="page"
-                                            href="/about"
-                                        >
-                                            About
-                                        </a>
-                                    </li>
-                                    <li className="nav-item">
-                                        <a
-                                            className="nav-link"
-                                            aria-current="page"
-                                            href="/team"
-                                        >
-                                            Team
-                                        </a>
-                                    </li>
-                                    <li className="nav-item-login">
-                                        <a
-                                            className="nav-link nav-link-login"
-                                            aria-current="page"
-                                            href="/logout"
-                                        >
-                                            Log Out
-                                        </a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </nav>
-                    </header>
-                </div>
-            </>
+                  <div
+                    className="collapse navbar-collapse"
+                    id="navbarSupportedContent"
+                  >
+                    <ul className="navbar-nav ms-auto">
+                      <li className="nav-item">
+                        <a
+                          className="nav-link"
+                          aria-current="page"
+                          href="/home"
+                        >
+                          Home
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a
+                          className="nav-link"
+                          aria-current="page"
+                          href="/events"
+                        >
+                          Events
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a
+                          className="nav-link"
+                          aria-current="page"
+                          href="/locations"
+                        >
+                          Locations
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a
+                          className="nav-link"
+                          aria-current="page"
+                          href="/locations"
+                        >
+                          My Profile
+                        </a>
+                      </li>
+                      <li className="nav-item">
+                        <a className="nav-link" aria-current="page" href="/">
+                          Log Out
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </nav>
+              </header>
+            </div>
+          </>
         );
     } else {
         return (
