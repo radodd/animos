4ece32c07dae1e30820bccb00fd2a22a1e8f4fa9
PA <epinette.pa@gmail.com>
2023-07-26 16:54:57 -0700

    Arggghhh

--- api/tests/test_accounts.py
+++ api/tests/test_accounts.py
@@ -5,7 +5,6 @@ from queries.accounts import AccountQueries
 
 client = TestClient(app)
 
-
 class AccountOut(BaseModel):
     id: str
     first_name: str
@@ -19,8 +18,8 @@ class AccountOut(BaseModel):
     hosted_events: list
     attending_events: list
 
-
 def fake_account_create(self, info, hashed_password):
+    # Simulate account creation
     account_data = {
         "id": "fake_id",
         "first_name": info.first_name,
@@ -36,10 +35,11 @@ def fake_account_create(self, info, hashed_password):
     }
     return AccountOut(**account_data)
 
-
 def test_create_account():
+    # Arrange
     app.dependency_overrides[AccountQueries] = fake_account_create
 
+    # Test data for account creation
     data = {
         "email": "test@example.com",
         "first_name": "John",
@@ -49,12 +49,22 @@ def test_create_account():
         "picture_url": "https://example.com/pic.jpg",
     }
 
+    # Act
     response = client.post("/api/accounts", json=data)
 
+    # Clean up
     app.dependency_overrides = {}
 
+    # Assert
     assert response.status_code == 200
     assert "id" in response.json()
     assert response.json()["first_name"] == data["first_name"]
     assert response.json()["last_name"] == data["last_name"]
     assert response.json()["email"] == data["email"]
+    assert response.json()["zipcode"] == data["zipcode"]
+    assert response.json()["picture_url"] == data["picture_url"]
+    assert len(response.json()["follower_list"]) == 0
+    assert len(response.json()["following_list"]) == 0
+    assert len(response.json()["pets"]) == 0
+    assert len(response.json()["hosted_events"]) == 0
+    assert len(response.json()["attending_events"]) == 0
