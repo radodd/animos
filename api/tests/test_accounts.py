cf6c3ce537f9468bce4ab2b061857194890d81ba
PA <epinette.pa@gmail.com>
2023-07-26 15:32:58 -0700

    Rdy for merge 3

--- api/tests/test_accounts.py
+++ api/tests/test_accounts.py
@@ -1,24 +1,28 @@
+from mongomock import MongoClient
+import pytest
 from main import app
 from fastapi.testclient import TestClient
 from models import AccountIn
 
-client = TestClient(app)
+@pytest.fixture
+def mock_mongo_client():
+    return MongoClient()
 
 
-def test_create_account():
+def test_create_account(mock_mongo_client):
     client = TestClient(app)
     info = AccountIn(
-        email="el@moustachio.com",
-        first_name="Pablo",
-        last_name="Escobar",
+        email="example@example.com",
+        first_name="John",
+        last_name="Doe",
         password="password",
-        zipcode="62150",
-        picture_url="https://moustache.com/picture.jpg",
-        follower_list=["Michou", "Robert"],
-        following_list=["Patoche", "Germaine"],
-        pets=["Lizard", "Koala"],
-        hosted_events=["Pool partayyy", "Snowfight"],
-        attending_events=["Catnip session", "Couch scratching"]
+        zipcode="12345",
+        picture_url="https://example.com/picture.jpg",
+        follower_list=["user1", "user2"],
+        following_list=["user3", "user4"],
+        pets=["dog", "cat"],
+        hosted_events=["event1", "event2"],
+        attending_events=["event3", "event4"]
     )
 
     response = client.post("/api/accounts", json=info.dict())
