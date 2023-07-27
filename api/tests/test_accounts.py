94702ffa060f82c13b2d71864090ea7734b43e7d
PA <epinette.pa@gmail.com>
2023-07-26 17:26:31 -0700

    Arggsdfsdfsdfsfdsdfsfs

--- api/tests/test_accounts.py
+++ api/tests/test_accounts.py
@@ -1,7 +1,7 @@
 from pydantic import BaseModel
 from fastapi.testclient import TestClient
 from main import app
-from queries.accounts import AccountQueries
+from typing import List, Optional
 
 client = TestClient(app)
 
@@ -11,71 +11,29 @@ class AccountOut(BaseModel):
     first_name: str
     last_name: str
     email: str
-    zipcode: str
-    picture_url: str
-    follower_list: list
-    following_list: list
-    pets: list
-    hosted_events: list
-    attending_events: list
+    password: str
+    zipcode: Optional[str]
+    picture_url: Optional[str]
+    follower_list: Optional[List[str]]
+    following_list: Optional[List[str]]
+    pets: Optional[List[str]]
+    hosted_events: Optional[List[str]]
+    attending_events: Optional[List[str]]
 
 
-class AccountTokenResponse(BaseModel):
-    account: AccountOut
-    token_type: str
-    access_token: str
-
-
-def fake_account_create(self, info, hashed_password):
-    # Simulate account creation
-    account_data = {
-        "id": "fake_id",
-        "first_name": info.first_name,
-        "last_name": info.last_name,
-        "email": info.email,
-        "zipcode": info.zipcode,
-        "picture_url": info.picture_url,
-        "follower_list": [],
-        "following_list": [],
-        "pets": [],
-        "hosted_events": [],
-        "attending_events": [],
-    }
-    return AccountOut(**account_data)
-
-
-def test_create_account():
-    # Arrange
-    app.dependency_overrides[AccountQueries] = fake_account_create
-
-    # Test data for account creation
-    data = {
-        "email": "test@example.com",
-        "first_name": "John",
-        "last_name": "Doe",
-        "password": "testpassword",
-        "zipcode": "12345",
-        "picture_url": "https://example.com/pic.jpg",
-    }
-
-    # Act
-    response = client.post("/api/accounts", json=data)
-
-    # Clean up
-    app.dependency_overrides = {}
-
-    # Assert
-    assert response.status_code == 200
-    assert "account" in response.json()
-    assert response.json()["account"]["first_name"] == data["first_name"]
-    assert response.json()["account"]["last_name"] == data["last_name"]
-    assert response.json()["account"]["email"] == data["email"]
-    assert response.json()["account"]["zipcode"] == data["zipcode"]
-    assert response.json()["account"]["picture_url"] == data["picture_url"]
-    assert len(response.json()["account"]["follower_list"]) == 0
-    assert len(response.json()["account"]["following_list"]) == 0
-    assert len(response.json()["account"]["pets"]) == 0
-    assert len(response.json()["account"]["hosted_events"]) == 0
-    assert len(response.json()["account"]["attending_events"]) == 0
-    assert "token_type" in response.json()
-    assert "access_token" in response.json()
+def fake_get_current_account_data():
+    account = AccountOut(
+        id="user123",
+        first_name="John",
+        last_name="Doe",
+        email="john.doe@example.com",
+        password="hashed_password",
+        zipcode="12345",
+        picture_url="https://example.com/profile_picture.jpg",
+        follower_list=["user456", "user789"],
+        following_list=["user456"],
+        pets=["Fluffy", "Max"],
+        hosted_events=["event123", "event456"],
+        attending_events=["event789"]
+    )
+    return account.__dict__
